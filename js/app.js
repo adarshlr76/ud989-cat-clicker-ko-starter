5131b51da43ef59aeb4bf8217bd772891dabedd8
Adarsh <adarshlr76@gmail.com>
2018-01-30 11:00:11 +0530

    added seperate function catclicker

--- js/app.js
+++ js/app.js
@@ -1,17 +1,36 @@
-var ViewModel = function() {
+
+var Cat = function() {
 	this.name = ko.observable('pretty');
 	this.count = ko.observable(0);
-	this.level = ko.observable('new born');
+	//this.level = ko.observable('new born');
 	this.imageSrc = ko.observable('img/22252709_010df3379e_z.jpg');
-
-	this.incrementCounter = function () {
-		this.count(this.count() + 1);
-		if(this.count() > 10 && this.count() <= 25) {
-			this.level('infant');
-		}else if (this.count() > 25) {
-			this.level('teen');
+	this.nickNames = ko.observableArray( [
+		{name : 'blah1'},
+		{name :'blah2'},
+		{name : 'blah3'}
+		]);
+	this.title = ko.computed (function () {
+		var level;
+		var clicks = this.count();
+		if(clicks <= 10){
+			level = "new born";
+		}else if(clicks > 10 && clicks <= 25) {
+			level = "infant";
+		}else  {
+			level = "teen";
 		}
+		return level;
 		
+	},this);
+}
+
+var ViewModel = function() {
+	
+	this.CurrentCat = ko.observable(new Cat());
+	
+	this.incrementCounter = function () {
+		this.CurrentCat().count(this.CurrentCat().count() + 1);
+	
 	}
 	//console.log(this.name);
 } 
